e338f9c744487a72008c7208b915e0fc261c5d22
cemck <cem143@hotmail.de>
2024-10-14 18:20:13 +0200

    init

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>The software keyboard is shown after app is relaunched from background</Text>
       <StatusBar style="auto" />
+      <TextInput style={styles.textInput} showSoftInputOnFocus={false} />
     </View>
   );
 }
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    height: 40,
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
   },
 });
